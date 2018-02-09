0f2011c5a5ff52868d6711e681398901770719b4
涂海生 <tuhaisheng@soyoung.com tuhaisheng@soyoung.com tuhaisheng@soyoung.com>
2018-02-09 16:23:05 +0800

    Add 添加宏

--- SharedCarSuitsExtension/Common/Macros/SCThirdInfoConst.h
+++ SharedCarSuitsExtension/Common/Macros/SCThirdInfoConst.h
@@ -8,6 +8,10 @@
 
 #ifndef SCThirdInfoConst_h
 #define SCThirdInfoConst_h
-
+/*
+ App ID：e3cc8d215f      App Key：8328ba7a-567e-4462-aac2-5695084b3751
+ */
+#define BuglyAppId @"e3cc8d215f"
+#define BuglyAppKey @"8328ba7a-567e-4462-aac2-5695084b3751"
 
 #endif /* SCThirdInfoConst_h */

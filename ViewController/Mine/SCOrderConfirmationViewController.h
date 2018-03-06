e9f68f78aa1dd22898e2ea19a7ae2564ba830cc5
tuhaisheng <2422606568@qq.com>
2018-03-06 20:26:19 +0800

    add README

---
+++ ViewController/Mine/SCOrderConfirmationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCOrderConfirmationViewController.h
+//  SharedCarSuits
+//
+//  Created by tuhaisheng on 2018/3/4.
+//  Copyright © 2018年 tuhaisheng. All rights reserved.
+//
+
+#import "SCBaseViewController.h"
+
+@interface SCOrderConfirmationViewController : SCBaseViewController
+
+@end
---
+++ ViewController/Mine/SCOrderConfirmationViewController.m
@@ -0,0 +1,37 @@
+//
+//  SCOrderConfirmationViewController.m
+//  SharedCarSuits
+//
+//  Created by tuhaisheng on 2018/3/4.
+//  Copyright © 2018年 tuhaisheng. All rights reserved.
+//
+
+#import "SCOrderConfirmationViewController.h"
+
+@interface SCOrderConfirmationViewController ()
+
+@end
+
+@implementation SCOrderConfirmationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
